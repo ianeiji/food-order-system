dba901a2b11575da1648ebc0889447e5a5181430
ianeiji <tajima830@gmail.com>
2024-05-25 22:24:03 +0900

    ✨ [server] Swaggerの設定を追加

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,14 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true })); // class transformerとclass validatorを有効にする
-  app.enableCors({ origin: ['http://localhost:3000'] });
+
+  const allowedOrigins = process.env.ALLOWED_ORIGINS.split(',');
+
+  app.enableCors({
+    origin: allowedOrigins,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   app.use(helmet());
 
-  await app.listen(3002);
+  const config = new DocumentBuilder()
+    .setTitle('Food Order System API')
+    .setDescription('API documentation for the Food Order System')
+    .setVersion('1.0')
+    .addTag('admin')
+    .addTag('order')
+    .addTag('kitchen')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3001);
 }
 bootstrap();
