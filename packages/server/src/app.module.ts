35bac46881c3698bd91a34031f5ae131d0a29864
ianeiji <tajima830@gmail.com>
2024-05-30 08:40:55 +0900

    ✨ [order-server] SessionServiceをProductモジュールに直接注入

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -2,16 +2,12 @@ import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { ProductModule } from './modules/order-system/product/product.module';
 import { SessionMiddleware } from './modules/session/session.middleware';
-import { SessionService } from './modules/session/session.service';
-import { SessionController } from './modules/session/session.controller';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     ProductModule, // this will make the .env variables available to the entire app
   ],
-  providers: [SessionService],
-  controllers: [SessionController],
 })
 export class AppModule {
   configure(consumer: MiddlewareConsumer) {
