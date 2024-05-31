68e04726592483a966ce6209a740318b44247c66
ianeiji <tajima830@gmail.com>
2024-05-31 20:37:17 +0900

    ✨ [order-server] セッション管理の処理を調整 - コミット漏れ

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -2,11 +2,13 @@ import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { ProductModule } from '@modules/order-system/product/product.module';
 import { SessionMiddleware } from '@modules/session/session.middleware';
+import { SessionModule } from '@modules/session/session.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     ProductModule, // this will make the .env variables available to the entire app
+    SessionModule,
   ],
 })
 export class AppModule {
